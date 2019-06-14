711a3ec8679ad92f14edbc512002c15ff36bea76
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-06-15 02:13:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver as wbd
+from selenium.common.exceptions import NoSuchElementException
 import pyautogui as pog
 from bs4 import BeautifulSoup as bs
 import requests
@@ -39,17 +40,26 @@ for link in links:
 
     if link.text in shows:
 
-        print(link.text)
+        print("\n", link.text, sep=""
+                                   "")
         browser.get("https://horriblesubs.info" + link.get("href"))
 
-        browser.find_element_by_css_selector('#hs-search > input').send_keys('11')
+        browser.find_element_by_css_selector('#hs-search > input').send_keys('12')
         pog.press('enter')
 
         sleep(1)
-        browser.find_element_by_css_selector(r'.rls-label').click()
+        try:
+            browser.find_element_by_css_selector(r'.rls-label').click()
+        except NoSuchElementException:
+            print("Episode not yet released")
+            continue
 
         sleep(1)
-        browser.find_element_by_css_selector(r'#\31 1-1080p > span:nth-child(2) > a:nth-child(1)').click()
+        try:
+            browser.find_element_by_css_selector(r'#\31 2-1080p > span:nth-child(2) > a:nth-child(1)').click()
+        except NoSuchElementException:
+            print("Episode not yet released")
+            continue
 
         sleep(1)
         pog.click(780, 522)
@@ -67,3 +77,5 @@ for link in links:
         sleep(2)
         pog.hotkey('alt', 'f4')
         #break
+
+browser.close()
