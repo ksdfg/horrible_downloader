6910f562b13091ea12042a9048bec013edb67621
ksdfg <ksdfg123@gmail.com>
2019-06-14 21:56:19 +0530

    sometimes working, sometimes not

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 import requests
+import time
 
+xpath = {
+    'search': r'/html/body/div[2]/div/div['r'2]/div[2]/div['r''r'1]/div/main/div['r''r'1]/article/div'r'/div['r''r'4]/form/input',
+    'episode': r'/html/body/div[2]/div/div['r'2]/div[2]/div['r''r'1]/div/main/div['r'1]/article/div/div['r'4]/div['r''r'1]/div/a',
+    'magnet': r'/html/body/div[2]/div/div['r'2]/div[2]/div['r''r'1]/div/main/div['r'1]/article/div/div['r'4]/div['r''r''r'1]/div/div'r'/div['r'3]/span[2]/a '
+         }
 
-def waitForElem(xpath, elem):
-    element_present = EC.presence_of_element_located((By.XPATH, xpath))
-
-    try:
-        WebDriverWait(driver, 10).until(element_present)
-    except TimeoutException:
-        print("problem in ", elem)
-
-
-shows = ["Fruits Basket (2019)", "Senryuu Shoujo", "Midara na Ao-chan wa Benkyou ga Dekinai", "Hitoribocchi no "
-                                                                                              "Marumaru Seikatsu"]
+shows = [
+    "Fruits Basket (2019)",
+    "Senryuu Shoujo",
+    "Midara na Ao-chan wa Benkyou ga Dekinai",
+    "Hitoribocchi no arumaru Seikatsu",
+    "Black Clover",
+    "Isekai Quartet",
+    "Sewayaki Kitsune no Senko-san",
+    "Tate no Yuusha no Nariagari",
+    "Kenja no Mago",
+    "Amazing Stranger",
+    "Joshikausei",
+    "Nobunaga-sensei no Osanazuma",
+    "Kimetsu no Yaiba",
+    "Bokutachi wa Benkyou ga Dekinai"
+]
 
 soup = bs(requests.get(
     "https://horriblesubs.info/").text, features='html.parser')
@@ -28,20 +39,34 @@ links = [i for i in soup.select('a[title = "See all releases for this show"]') i
 driver = wbd.Firefox()
 
 for link in links:
-    
+
     if link.text in shows:
         print(link.get("href"))
         driver.get("https://horriblesubs.info" + link.get("href"))
 
-        waitForElem(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div['r'1]/article/div/div[4]/form/input', "search")
-        b = driver.find_element_by_xpath(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div['r'1]/article/div/div[4]/form/input')
+
+        try:
+            WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, xpath['search'])))
+        except TimeoutException:
+            print("problem in", "search")
+
+        #time.sleep(15)
+        b = driver.find_element_by_xpath(xpath['search'])
         b.send_keys("1"+Keys.ENTER)
 
-        waitForElem(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div[1]/article/div/div['r'4]/div[1]/div/a', "episode")
-        b = driver.find_element_by_xpath(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div[1]/article/div/div['r'4]/div[1]/div/a')
+        try:
+            WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, xpath['episode'])))
+        except TimeoutException:
+            print("problem in", "episode")
+        #time.sleep(2)
+        b = driver.find_element_by_xpath(xpath['episode'])
         b.click()
 
-        waitForElem(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div[1]/article/div/div['r'4]/div['r'1]/div/div'r'/div[3]/span[2]/a', "magnet")
-        b = driver.find_element_by_xpath(r'/html/body/div[2]/div/div[2]/div[2]/div[1]/div/main/div[1]/article/div/div['r'4]/div['r'1]/div/div'r'/div[3]/span[2]/a')
+        try:
+            WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, xpath['magnet'])))
+        except TimeoutException:
+            print("problem in", "episode")
+        #time.sleep(2)
+        b = driver.find_element_by_xpath(xpath['magnet'])
         b.click()
         #break
