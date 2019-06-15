b4293eec58f622a49926ab681e041ced44625662
Aditya <43269300+AdityaDesai-99@users.noreply.github.com>
2019-06-15 09:38:43 +0530

    Modified qbittorrent() function

--- horrible_functions.py
+++ horrible_functions.py
@@ -1,15 +1,15 @@
 from selenium import webdriver as wbd
 from time import sleep
 import pyautogui as pog
+from user_preferences import preferences as pf
 
-# Dictionary of web drivers according to browser
+
 drivers = {
     'firefox': wbd.Firefox,
     'chrome': wbd.Chrome
 }
 
 
-# A function that returns the element which, when clicked, brings links of episode to be downloaded in view
 def episode_selector(webdriver, ep, browser):
     if browser == 'firefox':
         return webdriver.find_element_by_css_selector(r'.rls-label')
@@ -17,7 +17,6 @@ def episode_selector(webdriver, ep, browser):
         return webdriver.find_element_by_xpath('//*[@id="' + ep + '"]/a')
 
 
-# A funtion that returns the element which, when clicked, opens magnet link
 def magnet_selector(webdriver, ep, quality, browser):
     if browser == 'firefox':
         return webdriver.find_element_by_css_selector(r'#\3' + ep[0] + ' ' + ep[1:] + '-' + quality + '> span:nth-child(2) > ''a:nth-child(1)')
@@ -25,25 +24,27 @@ def magnet_selector(webdriver, ep, quality, browser):
         return webdriver.find_element_by_xpath('//*[@id="' + ep + '-1080p"]/span[2]/a')
 
 
-# Function for when magnet link is opened in utorrent
 def utorrent_download(path):
     sleep(3)
-    pog.typewrite(path)     # enter path where you want to store the downloaded episode
+    pog.typewrite(path)  # enter path where you want to store the downloaded episode
     pog.press('enter')
     sleep(5)
     pog.press('enter')
 
 
-# Function for when magnet link is opened in qbittorrent
-def qbittorrent_download(path):
+def qbittorrent_download(i,path):
+    sleep(3)
+    pog.click(*pf['clicks'][i])
+    i += 1
     sleep(2)
-    pog.click(1020, 216)
-    pog.hotkey('ctrl', 'a')
+    pog.click(*pf['clicks'][i])
+    i += 1
     pog.typewrite(path)     # enter path where you want to store the downloaded episode
     pog.press('enter')
     sleep(1)
     pog.press('enter')
-
+    pog.press('enter')
+    sleep(2)
 
 torrents = {
     'utorrent': utorrent_download,
