70a449dd501e1d0aaddedf219ef75158eaa71d2e
ksdfg <ksdfg123@gmail.com>
2019-06-15 20:59:29 +0530

    added comments, made a few changes in horrible_downloader.py

--- currently_watching.py
+++ currently_watching.py
@@ -1,23 +1,21 @@
 # This is the list of shows you are currently watching this season
-# The syntax is r"show's name": next episode to be downloaded
-# e.g. : r"JoJo's Bizarre Adventure - Golden Wind": 35
+# The syntax is "show's name": next episode to be downloaded
+# e.g. : "JoJo's Bizarre Adventure - Golden Wind": 35
 # This means that I already have downloaded ep. 34 and need to download ep.35
 # Please ensure that the show's name is exactly the same as it appears in HorribleSubs' schedule
 
-shows = {
-    r"Fruits Basket (2019)": 12,
-    r"Senryuu Shoujo": 12,
-    r"Midara na Ao-chan wa Benkyou ga Dekinai": 12,
-    r"Hitoribocchi no Marumaru Seikatsu": 12,
-    r"Black Clover": 88,
-    r"Isekai Quartet": 11,
-    r"Sewayaki Kitsune no Senko-san": 10,
-    r"Tate no Yuusha no Nariagari": 24,
-    r"Kenja no Mago": 11,
-    r"Amazing Stranger": 12,
-    r"Joshikausei": 12,
-    r"Nobunaga-sensei no Osanazuma": 12,
-    r"Kimetsu no Yaiba": 12,
-    r"Bokutachi wa Benkyou ga Dekinai": 12,
-    r"One Punch Man S2": 10
-}
+shows = {'Fruits Basket (2019)': 12,
+'Senryuu Shoujo': 12,
+'Midara na Ao-chan wa Benkyou ga Dekinai': 12,
+'Hitoribocchi no Marumaru Seikatsu': 12,
+'Black Clover': 88,
+'Isekai Quartet': 11,
+'Sewayaki Kitsune no Senko-san': 10,
+'Tate no Yuusha no Nariagari': 24,
+'Kenja no Mago': 11,
+'Amazing Stranger': 12,
+'Joshikausei': 12,
+'Nobunaga-sensei no Osanazuma': 12,
+'Kimetsu no Yaiba': 11,
+'Bokutachi wa Benkyou ga Dekinai': 11,
+'One Punch Man S2': 10}
