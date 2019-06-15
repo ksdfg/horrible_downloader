030cea816ba44d25e2093bca284cef27d10fd2c8
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-06-15 06:48:58 +0530

    making all preferences blank
    need to fill these first when setting up

--- user_preferences.py
+++ user_preferences.py
@@ -2,21 +2,20 @@
 
 preferences = {
     # The browser that the user prefers to use
-    'browser': 'firefox',
+    'browser': '',
 
     # The path of the corresponding web driver (location where user has downloaded it)
-    'driver_path': r"C:\Users\ksdfg\AppData\Local\Programs\Python\Python37\Lib\site-packages"r"\selenium\webdriver"
-                   r"\firefox\geckodriver.exe",
+    'driver_path': '',
 
     # the torrent downloading software to be used
-    'torrent': 'utorrent',
+    'torrent': '',
 
     # path where all anime is to be downloaded
-    'download_path': 'K:\\Videos\\',
+    'download_path': '',
 
     # Quality in which user wants to download episode
-    'quality': '1080p',
+    'quality': '',
 
     # list of all coordinates where the program must click during operation
-    'clicks': [[780, 522]]
+    'clicks': []
 }
