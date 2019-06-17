8ac99d61c32493e846021216f865992bfe56c75a
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-06-17 18:26:17 +0530

    Update user_preferences.py

--- user_preferences.py
+++ user_preferences.py
@@ -2,17 +2,17 @@
 
 preferences = {
     # The browser that the user prefers to use
-    'browser': '',
+    'browser': ' ',
 
     # The path of the corresponding web driver (location where user has downloaded it)
-    'driver_path': r'',
+    'driver_path': r' ',
 
     # the torrent downloading software to be used
-    'torrent': '',
+    'torrent': ' ',
 
     # path where all anime is to be downloaded
-    'download_path': '',
+    'download_path': ' ',
 
     # Quality in which user wants to download episode
-    'quality': ''
+    'quality': ' '
 }
