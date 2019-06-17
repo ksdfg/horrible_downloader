6e0f42522ace73f3dcca17e811fba226cd539681
Aditya Desai <AdityaDesai-99@users.noreply.github.com>
2019-06-17 18:57:05 +0530

    testing github

--- user_preferences.py
+++ user_preferences.py
@@ -2,17 +2,17 @@
 
 preferences = {
     # The browser that the user prefers to use
-    'browser': ' ',
+    'browser': 'chrome',
 
     # The path of the corresponding web driver (location where user has downloaded it)
-    'driver_path': r' ',
+    'driver_path': r'E:\Applications\chromedriver.exe',
 
     # the torrent downloading software to be used
-    'torrent': ' ',
+    'torrent': 'qbittorrent',
 
     # path where all anime is to be downloaded
-    'download_path': ' ',
+    'download_path': 'F:\\Anime\\',
 
     # Quality in which user wants to download episode
-    'quality': ' '
+    'quality': '1080p'
 }
