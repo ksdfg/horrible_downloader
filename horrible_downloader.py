07a5d2ed08a4dec43102f9310a6f9f39e0d86387
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-07-06 21:44:39 +0530

    Update horrible_downloader.py
    for when it is called by scheduler

--- horrible_downloader.py
+++ horrible_downloader.py
@@ -1,5 +1,32 @@
 # main menu
 
+import sys
+import ctypes
+from threading import Thread
+from time import sleep
+import os
+from pyautogui import getWindowsWithTitle
+
+# if it has been called by scheduler
+if len(sys.argv) > 1:
+    i = -1
+    
+    def meow():    # function to thread - wait for 10s, then start check
+        sleep(10)
+        if i==-1:
+            getWindowsWithTitle('horrible downloader')[0].close()
+            os.system('python "' + os.path.join(os.path.expandvars('%horriblehome%'), 'horriblefiles', 'ongoing_downloader.py') + '" arg')
+    
+    t = Thread(target=meow)
+    t.start()
+    
+    i = ctypes.windll.user32.MessageBoxW(0, "Start checks of currently watching anime?", "horrible downloader", 0x1001)  # popup
+    if i == 1:
+        os.system('python "' + os.path.join(os.path.expandvars('%horriblehome%'), 'horriblefiles', 'ongoing_downloader.py') + '" arg')
+        
+    t.join()
+    exit(0)
+
 print('\n1. Check and download new episodes of anime in your currently watching list\t(type 1 to select this)'
       '\n2. Batch download of episodes of an anime\t(type 2 to select this)'
       '\n3. Update currently watching list\t(type 3 to select this)'
