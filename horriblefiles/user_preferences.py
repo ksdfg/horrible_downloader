0a905ab0fde2a4be88c1bdde06936cda625f4b1a
ksdfg <ksdfg123@gmail.com>
2019-07-29 02:16:02 +0530

    changed files for a system with no selenium

--- horriblefiles/user_preferences.py
+++ horriblefiles/user_preferences.py
@@ -1,12 +1,6 @@
 # This file stores the preferences of the user
 
 preferences = {
-    # The browser that the user prefers to use
-    'browser': ' ',
-
-    # The path of the corresponding web driver (location where user has downloaded it)
-    'driver_path': r' ',
-
     # the torrent downloading software to be used
     'torrent': ' ',
 
